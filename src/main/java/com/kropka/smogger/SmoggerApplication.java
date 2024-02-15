1dc7fc8aa80e0b93b1db589b8fbce708f367bba6
Katarzyna Kropiak <k.kropiak@gmail.com>
2024-02-15 21:47:20 +0100

    Initial Commit

---
+++ src/main/java/com/kropka/smogger/SmoggerApplication.java
@@ -0,0 +1,13 @@
+package com.kropka.smogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kropka/smogger/SmoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kropka.smogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
